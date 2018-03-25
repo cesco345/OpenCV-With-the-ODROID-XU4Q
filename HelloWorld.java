53dde3a4ab652e826751abebb2f60b27d171c596
Francesco Piscani <stem.apks@gmail.com>
2018-03-25 18:23:13 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *
+ ******************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
